5c1101f0a9c5307e34b1f15329b984996c4a3560
trevorddooley <98045078+trevorddooley@users.noreply.github.com>
2022-11-03 14:19:22 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,49 @@
+/*import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+}
+*/ 
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Welcome from './pages/Welcome';
+import MemberSign from './pages/MemberSign';
+import MemberResult from './pages/MemberResult';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="WelcomeScreen"
+          component={Welcome}
+          // options={{headerShown: false}} //Sayfanın ustunde cıkan Başlığımızı gizledik.
+        />
+        <Stack.Screen name="MemberSignScreen" component={MemberSign} />
+        <Stack.Screen name="MemberResultScreen" component={MemberResult} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
