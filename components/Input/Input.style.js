b97dece897e940786864eddd4c5e8446c86732e6
trevorddooley <98045078+trevorddooley@users.noreply.github.com>
2022-11-03 14:16:26 -0500

    Create Input.style.js

---
+++ components/Input/Input.style.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  input_container: {
+    borderWidth: 1,
+    margin: 5,
+    padding: 1,
+    borderRadius: 5,
+    borderColor: '#bdbdbd',
+  },
+  container: {
+    margin: 10,
+  },
+  label: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginLeft: 5,
+  },
+});
