b24435bb7194ccebe755fef542d11a5eac884652
trevorddooley <98045078+trevorddooley@users.noreply.github.com>
2022-11-03 14:10:00 -0500

    Create Success.js

---
+++ screens/Success.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+
+function Success({route}) {
+  const {animal} = route.params;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text style={styles.message}>Submission Complete!</Text>
+      <Text style={styles.label}>Species: {animal.animalSpecies}</Text>
+      <Text style={styles.label}>Age: {animal.animalAge}</Text>
+      <Text style={styles.label}>Date and Time: {animal.dateTime}</Text>
+      <Text style={styles.label}>Deceased: {animal.deceased}</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 5,
+  },
+  message: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    textAlign: 'center',
+    color: '#9fa8da',
+  },
+});
+
+export default Success;
