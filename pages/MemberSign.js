9428de78b3ab9b2f332a9c1ede8f91633f1875ab
trevorddooley <98045078+trevorddooley@users.noreply.github.com>
2022-11-17 13:59:42 -0600

    Update MemberSign.js

--- pages/MemberSign.js
+++ pages/MemberSign.js
@@ -1,16 +1,16 @@
 import React, {useState} from 'react';
 //import * as React from 'react';
-import {View, Alert} from 'react-native';
+import {View, Alert, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import { StyleSheet, Text, SafeAreaView, Dimensions  } from 'react-native';
 import MapView from 'react-native-maps';
 import DocumentPicker from 'react-native-document-picker';
-
-
+import DatePicker from 'react-native-date-picker';
 
 const STYLES = ['default', 'dark-content', 'light-content'];
 const TRANSITIONS = ['fade', 'slide', 'none'];
+const deviceHeight = Dimensions.get("window").height;
 
 
 
@@ -18,81 +18,166 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#D3D3D3',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    height: deviceHeight,
+  },
+  speciesTB: {
+    position:"absolute",
+    bottom: 300,
+    left: 10,
+
+  },
+  ageTB:{
+    position: "absolute",
+    bottom: 300,
+    right: 10,
+  },
+  dateTB:{
+    position: "absolute",
+    bottom: 230,
+    left: 10,
+  },
+  timeTB:{
+    position: "absolute",
+    bottom: 230,
+    right: 10,
+  },
+  deathTB:{
+    position: "absolute",
+    bottom: 160,
+    right: 10,
+  },
+  locationTB:{
+    position: "absolute",
+    bottom: 160,
+    left: 10,
+  },
+  photoTB:{
+    position: "absolute",
+    bottom: 88,
+    left: 100,
   },
   map: {
-    width: '100%',
-    height: '100%',
+    height: '61%',
   },
 });
 
 function MemberSign({navigation}) {
   const [animalSpecies, setAnimalSpecies] = useState(null);
   const [animalAge, setAnimalAge] = useState(null);
-  const [dateTime, setDateTime] = useState(null);
+  const [date, setDate] = useState(null);
   const [deceased, setDeceased] = useState(null);
+  const [time, setTime] = useState(null);
+  const [location, setLocation] = useState(null);
+
 
   function handleSubmit() {
-    if (!animalSpecies || !animalAge || !dateTime || !deceased) {
-      Alert.alert('Submission Failed', 'One or more fields is incorrect.');
+    /*if (!animalSpecies || !animalAge || !date || !deceased || !time || !location) {
+      Alert.alert('Submission Failed', 'One or more fields is missing or incorrect.');
       return;
-    }
+    }*/
 
 
     const animal = {
       animalSpecies,
       animalAge,
-      dateTime,
+      date,
       deceased,
+      time,
+      location,
     };
   
   navigation.navigate('MemberResultScreen', {animal});
   }
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <Text>Member Sign</Text> */}
-      <View style={styles.container}>
-        <MapView style={styles.map}
-          initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0,
-              longitudeDelta: 0.0,
-          }}
-        >
-        <MapView.Marker
-            coordinate={{latitude: 37.78825,
-            longitude: -122.4324}}
-            title={"title"}
-            description={"description"}
-         />
-      </MapView>
-      </View>
-      
-      <Input
-        label="Species"
-        placeholder="Insert species..."
-        onChangeText={setAnimalSpecies}
-      />
-      <Input
-        label="Age"
-        placeholder="Approximate age..."
-        onChangeText={setAnimalAge}
-      />
-      <Input
-        label="Date and Time"
-        placeholder="Select the date and time..."
-        onChangeText={setDateTime}
-      />
-      <Input
-        label="Deceased?"
-        placeholder="Yes or No..."
-        onChangeText={setDeceased}
-      />
-      <Button text="Complete Submission" onPress={handleSubmit} />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{flex: 1}}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ flex: 1 }}>
+        {/* <Text>Member Sign</Text> */}
+          <View style = {{flex: 1}}>
+            <MapView style={styles.map}
+              initialRegion={{
+                latitude: 37.78825,
+                longitude: -98.4324,
+                latitudeDelta: 0.0,
+                longitudeDelta: 0.0,
+              }}
+            >
+            <MapView.Marker
+              coordinate={{latitude: 37.0902,
+              longitude: -98.4324}}
+              title={"title"}
+              description={"description"}
+            />
+            
+            </MapView>
+          </View>
+
+        <View style={styles.speciesTB}>
+          <Input
+            label="Species"
+            placeholder="  Insert species...   "
+            placeholderTextColor="#fff"
+            onChangeText={setAnimalSpecies}
+          />
+        </View>
+
+        <View style={styles.ageTB}>
+          <Input
+            label="Age"
+            placeholder="  Approximate age...  "
+            onChangeText={setAnimalAge}
+          />
+        </View>
+
+        <View style={styles.dateTB}>
+          <Input
+            label="Date"
+            placeholder="  MM/DD/YYYY  "
+            onChangeText={setDate}
+          />
+        </View>
+
+        <View style={styles.deathTB}>
+          <Input
+            label="Deceased?"
+            placeholder="  Yes or No...  "
+            onChangeText={setDeceased}
+            />
+        </View>
+        <View style={styles.timeTB}>
+          <Input
+            label="Time"
+            placeholder="  HH:MM:SS  "
+            onChangeText={setTime}
+          />
+        </View>
+        <View style={styles.locationTB}>
+          <Input
+            label="Location"
+            placeholder="  Input location or move pin...  "
+            onChangeText={setLocation}
+            />
+        </View>
+        <View style={styles.photoTB}>
+          <Input
+            label="Upload"
+            placeholder="  Upload will live here  "
+            
+            //onChangeText={setPhoto}
+            />
+        </View>
+
+        <SafeAreaView>
+          <Button text="Complete Submission" onPress={handleSubmit} />
+        </SafeAreaView>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView> 
   );
 }
 
