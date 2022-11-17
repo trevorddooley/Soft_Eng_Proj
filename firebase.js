7a39a2dc919dd424dc3d8a32dc48e50c15928d18
trevorddooley <98045078+trevorddooley@users.noreply.github.com>
2022-11-17 13:58:39 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-/*
+
 // Import the functions you need from the SDKs you need
 import * as firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,5 +25,5 @@ if (firebase.apps.length === 0) {
 const auth = firebase.auth()
 
 export { auth };
-*/
+
 
