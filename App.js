2f7cd3a3f43bc15bae8a5d24928058eb13dc861a
trevorddooley <98045078+trevorddooley@users.noreply.github.com>
2022-12-06 12:56:49 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
+//Import dependencies
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+//Import files needed to creat navigation stack
 import Welcome from './pages/Welcome';
-import MemberSign from './pages/MemberSign';
-import MemberResult from './pages/MemberResult';
 import LoginScreen from './screens/LoginScreen';
+import FirebaseTest from './pages/FirebaseTest';
+import FirebaseResult from './pages/FirebaseResult';
 
+//declare stack and create navigation
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen
-          name="WelcomeScreen"
-          component={Welcome}
-          // options={{headerShown: false}}
-        />
-        <Stack.Screen name="MemberSignScreen" component={MemberSign} />
-        <Stack.Screen name="MemberResultScreen" component={MemberResult} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="FirebaseTestScreen" component={FirebaseTest} />
+        <Stack.Screen name="FirebaseResultScreen" component={FirebaseResult} />
+        <Stack.Screen name="WelcomeScreen" component={Welcome}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
